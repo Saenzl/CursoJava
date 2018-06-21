3dac81a75d53a261dd00a915d0ef096f41a30281
orlando <orlandolozaarr@gmail.com>
2018-06-21 09:37:41 -0500

    clase 4

---
+++ clases/clase4/src/inheritance/Child.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+public class Child extends Parent{
+	
+	
+	/*
+	 * Overriding
+	 */
+	
+	
+	@Override
+	public void sum(int a, int b){
+		System.out.println(a+b);
+	}
+	
+	public String childMethod(){
+		return "from class Child";
+	}
+	
+}
